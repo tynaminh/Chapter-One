c9959c321cbf22272dfa15ad1ea4fcc66f1dff00
Tyna Nguyen <tn182848@students.davenport.k12.ia.us>
2019-11-05 14:06:40 -0600

    update chapter 1

---
+++ Wine Glass/Wine Glass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wine_Glass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a wine glass graphic
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("X                 x");
+            Console.WriteLine(" X               x");
+            Console.WriteLine("  XXXXXXXXXXXXXXX");
+            Console.WriteLine("   XXXXXXXXXXXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine("       XXXX");
+            Console.WriteLine(" XXXXXXXXXXXXXXXXXX");
